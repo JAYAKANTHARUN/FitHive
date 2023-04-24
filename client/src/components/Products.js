8323116b638be75aab03da7c18a233a303bc0934
Jayakanth Arun <jayakantharun03@gmail.com>
2023-04-24 22:28:13 +0530

    Added Search Bar

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom"
 
 const Products = () => {
 
-    const navigate=useNavigate()
+    const navigate = useNavigate()
 
     const [products, setproducts] = useState([])
 
@@ -31,45 +31,67 @@ const Products = () => {
         }
     }
 
-    const updateproduct=(id)=>{
+    const updateproduct = (id) => {
         navigate(`/update/${id}`)
     }
-    const addproduct=()=>{
+    const addproduct = () => {
         navigate('/add')
+    }
+    const searchproduct = async (event) => {
+        let key = event.target.value
+        if (key) {
+            let result = await fetch(`http://127.0.0.1:3000/search/${key}`)
+            result = await result.json()
+            if (result) {
+                setproducts(result)
+            }
+        }
+        else {
+            getproducts()
+        }
+
     }
 
     return (
         <div className="products">
             <h1>Products List</h1>
-            <button className="addbutton" onClick={()=>addproduct()}>Add Product</button>
+            <button className="addbutton" onClick={() => addproduct()}>Add Product</button>
             <br /><br />
-            <table>
-                <thead>
-                    <tr>
-                        <th scope="col">Slno</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Price</th>
-                        <th scope="col">Category</th>
-                        <th scope="col">Company</th>
-                        <th scope="col">Operation</th>
+            <input className="searchbar" onChange={searchproduct} type="text" placeholder="Search Here" />
+            <br /><br />
+            <div className="producttable">
+                {
+                    products.length===0 ? <h3>No Products Found</h3>
+                    :
+                    <table>
+                    <thead>
+                        <tr>
+                            <th scope="col">Slno</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Category</th>
+                            <th scope="col">Company</th>
+                            <th scope="col">Operation</th>
 
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        products.map((item, index) =>
-                            <tr key={item._id}>
-                                <th scope="row">{index + 1}</th>
-                                <td>{item.name}</td>
-                                <td>{item.price}</td>
-                                <td>{item.category}</td>
-                                <td>{item.company}</td>
-                                <td><button onClick={() => deleteproduct(item._id)}>Delete</button><button onClick={()=>updateproduct(item._id)}>Update</button></td>
-                            </tr>
-                        )
-                    }
-                </tbody>
-            </table>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            products.map((item, index) =>
+                                <tr key={item._id}>
+                                    <th scope="row">{index + 1}</th>
+                                    <td>{item.name}</td>
+                                    <td>{item.price}</td>
+                                    <td>{item.category}</td>
+                                    <td>{item.company}</td>
+                                    <td><button onClick={() => deleteproduct(item._id)}>Delete</button><button onClick={() => updateproduct(item._id)}>Update</button></td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
+                </table>
+                }
+            </div>
         </div>
     )
 }
