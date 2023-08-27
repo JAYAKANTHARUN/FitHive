4ae8cdc1aeeb0baf212bb6d66df5120731300e83
Jayakanth Arun <jayakantharun03@gmail.com>
2023-08-27 14:38:22 +0530

    Added MongoDB cloud

--- server/db/config.js
+++ server/db/config.js
@@ -2,7 +2,15 @@ const mongoose=require('mongoose')
 
 mongoose.set('strictQuery', false);
 
-mongoose.connect('mongodb://127.0.0.1:27017/fithive',{useNewUrlParser:true,useUnifiedTopology:true},(err)=>{
+// mongoose.connect('mongodb://127.0.0.1:27017/fithive',{useNewUrlParser:true,useUnifiedTopology:true},(err)=>{
+//     if (err){
+//         console.log('error : '+err)
+//     }
+//     else{
+//         console.log('database connected successfully')
+//     }
+// })
+mongoose.connect('mongodb+srv://jayakantharun03:FitHive@cluster0.txiaeb6.mongodb.net/',{useNewUrlParser:true,useUnifiedTopology:true},(err)=>{
     if (err){
         console.log('error : '+err)
     }
